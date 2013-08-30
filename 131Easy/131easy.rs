c4f7d9ea16aed9b8d7aba2c9742e40a754b8165a
Kasey Carrothers <kaseyc.808@gmail.com>
2013-08-29 22:31:32 -0700

    Changed tabs to spaces

--- 131Easy/131easy.rs
+++ 131Easy/131easy.rs
@@ -1,37 +1,37 @@
 use std::{int, str, io};
 
 fn check_upper(s1: &str, s2: &str) -> bool {
-	s1.to_ascii().to_upper() == s2.to_ascii().to_owned()
+    s1.to_ascii().to_upper() == s2.to_ascii().to_owned()
 }
 
 fn check_reverse(s1: &str, s2: &str) -> bool {
-	let mut rev_str = ~"";
+    let mut rev_str = ~"";
 
-	for s1.rev_iter().advance |a| {
-		rev_str = rev_str + str::from_char(a);
-	}
-	
-	rev_str == s2.to_owned()
+    for s1.rev_iter().advance |a| {
+        rev_str = rev_str + str::from_char(a);
+    }
+    
+    rev_str == s2.to_owned()
 }
 
 fn main() {
-	let input = io::stdin().read_lines();
+    let input = io::stdin().read_lines();
 
-	let count;
-	match int::from_str(input[0]) {
-		Some(x) if x > 0 => count = x as uint,
-		_ => fail!("Must have a valid number of lines")
-	}
+    let count;
+    match int::from_str(input[0]) {
+        Some(x) if x > 0 => count = x as uint,
+        _ => fail!("Must have a valid number of lines")
+    }
 
-	let lines = input.slice(1, count+1);
+    let lines = input.slice(1, count+1);
 
-	for lines.iter().advance |line| {
-		let words = line.word_iter().collect::<~[&str]>();
+    for lines.iter().advance |line| {
+        let words = line.word_iter().collect::<~[&str]>();
 
-		match words[0] {
-			"0" if check_reverse(words[1], words[2]) => println("Good test data"),
-			"1" if check_upper(words[1], words[2]) => println("Good test data"),
-			_ => println("Mismatch! Bad test data")
-		}
-	}
+        match words[0] {
+            "0" if check_reverse(words[1], words[2]) => println("Good test data"),
+            "1" if check_upper(words[1], words[2]) => println("Good test data"),
+            _ => println("Mismatch! Bad test data")
+        }
+    }
 }
