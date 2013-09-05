8561bced32455d7c9081cf8988456c6d9b227569
Kasey Carrothers <kaseyc.808@gmail.com>
2013-09-05 16:52:11 -0700

    Game of Life: Updates the grid each tick according to the rules of the Game of Life

--- Game-of-Life/game_of_life.rs
+++ Game-of-Life/game_of_life.rs
@@ -1,4 +1,4 @@
-fn print_grid(row: &[bool], c: int) -> () {
+fn print_grid(row: &[bool], c: uint) -> () {
     let mut count = 0;
     for row.iter().advance |x| {
         match *x {
@@ -14,8 +14,58 @@ fn print_grid(row: &[bool], c: int) -> () {
     }
 }
 
+fn check_alive(grid: &[bool], index: uint, row_len: uint) -> bool {
+    let mut live_neighbors = 0;
+
+    if ((index % row_len) != 0 && index >= row_len) {
+        //Check status of upper left neighbor, if possible
+        if grid[index - row_len - 1] {live_neighbors += 1;}
+    }
+
+    if (index >= row_len) {
+        //Check status of upper neighbor, if possible
+        if grid[index - row_len] {live_neighbors += 1;}
+    }
+
+    if ((index % row_len) != row_len-1 && index >= row_len) {
+        //Check status of upper right neighbor, if possible
+        if grid[index - row_len + 1] {live_neighbors += 1;}
+    }
+
+    if ((index % row_len) != row_len-1) {
+        //Check status of right neighbor, if possible
+        if grid[index + 1] {live_neighbors += 1;}
+    }
+
+    if ((index % row_len) != row_len-1 && index < grid.len() - row_len) {
+        //Check status of lower right neighbor, if possible
+        if grid[index + row_len + 1] {live_neighbors += 1;}
+    }
+
+    if (index < grid.len() - row_len) {
+        //Check status of lower neighbor, if possible
+        if grid[index + row_len] {live_neighbors += 1;}
+    }
+
+    if ((index % row_len) != 0 && index < grid.len() - row_len) {
+        //Check status of lower left neighbor, if possible
+        if grid[index + row_len -1] {live_neighbors += 1;}
+    }
+
+    if ((index % row_len) != 0) {
+        //Check status of left neighbor, if possible
+        if grid[index - 1] {live_neighbors += 1;}
+    }
+
+    match grid[index] {
+        true if (live_neighbors == 2 || live_neighbors == 3) => true,
+        false if (live_neighbors == 3) => true,
+        _ => false
+    }
+}
+
 fn main() {
-    let count = 6;
+    let row_len = 6;
 
     let mut grid = ~[false, false, true, true, false, false,
                      false, true, false, false, true, false,
@@ -26,11 +76,21 @@ fn main() {
 
     loop {
         print("\x1B[2J\x1B[H");
-        print_grid(grid, count);
-        grid = grid.map(|x| !*x);
 
-        let mut  i = 0;
-        while (i < 300000000) {
+        print_grid(grid, row_len);
+
+        let mut index: uint = 0;
+        let mut new_grid: ~[bool] = ~[];
+
+        while index < grid.len() {
+            new_grid.push(check_alive(grid, index, row_len));
+            index += 1;
+        }
+
+        grid = new_grid;
+
+        let mut i = 0;
+        while (i < 250000000) {
             //Stopgap pause measure
             //TODO: Replace with sleep/timer func
             i += 1;
